6eafc3a8c08735d026e5e9dbcbfc2138d0a8c6c3
hyuk0u0 <hyuk0u0@naver.com>
2020-07-20 13:06:04 +0900

    첫번째 커밋

---
+++ Cmarket/src/main/java/kr/co/persistence/BoardDAOImpl.java
@@ -0,0 +1,7 @@
+package kr.co.persistence;
+
+public class BoardDAOImpl implements BoardDAO {
+	
+	private final String NS = "b.o.a";
+
+}
---
+++ Cmarket/src/main/java/kr/co/persistence/MemberDAOImpl.java
@@ -0,0 +1,7 @@
+package kr.co.persistence;
+
+public class MemberDAOImpl implements MemberDAO {
+	
+	private final String NS = "m.e.m";
+
+}
